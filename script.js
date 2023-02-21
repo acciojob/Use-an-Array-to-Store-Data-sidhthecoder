175e5b230b75d23df76b5e938c53a41d53cb9909
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-02-21 17:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-const yourArray = {
+let yourArray = {
   String: "John",
   String: "Doe",
   Number: 50,
   boolean: true,
   Number:60;
 };
-document.getElementById("show").textContent =yourArray;
+console.log(yourArray);
