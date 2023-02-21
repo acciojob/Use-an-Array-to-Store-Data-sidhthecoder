9584655a710e89fbfd43f44ddf6ce817808b9e49
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-02-21 17:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let simpleArray = ['one', 2, 'three', true, false, undefined, null]; 
-console.log(simpleArray.length);
-
 let complexArray = [ [ { one: 1, two: 2 }, 
 					  { three: 3, four: 4 } ]
 					, [ { a: "a", b: "b" }, 
 					   { c: "c", d: "d" } ] 
 				     ];
 let yourArray = ['thirty', 2007, true, 26, 'love'];
-document.getElementById("show").innerHTML=yourArray;
+document.getElementById("simpleArray").innerHTML=simpleArray.length;
+document.getElementById("complexArray").innerHTML=complexArray.length;
+document.getElementById("yourArray").innerHTML=yourArray.length;
