5a4d95e6d34a87e54dcfe3b9e17eea5b4a2848b7
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-02-21 17:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let yourArray;
 let yourArray = ['thirty', 2007, true, 26, 'love'];
-document.getElementById("show").textContent=yourArray;
+document.getElementById("show").innerHTML=yourArray.length;
